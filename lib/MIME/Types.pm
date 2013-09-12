7bded6c1ee8be42b7f46d32ded498529708e23ed
Mark Overmeer <mark@overmeer.net>
2013-09-12 15:46:29 +0200

    distribution MIME-Types-2.04.tar.gz

--- lib/MIME/Types.pm
+++ lib/MIME/Types.pm
@@ -121,7 +121,8 @@ sub _read_db($)
             }
         }
         else
-        {   while(<DB>)
+        {   local $_;
+            while(<DB>)
             {   last if m/^$/;
                 next if $skip_section;
                 chomp;
