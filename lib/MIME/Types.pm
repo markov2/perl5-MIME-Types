ca211534a32a7f2618b3259d70b065b8585c79db
Mark Overmeer <mark@overmeer.net>
2018-01-19 17:24:33 +0100

    convert to GIT

--- lib/MIME/Types.pm
+++ lib/MIME/Types.pm
@@ -1,3 +1,6 @@
+# This code is part of distribution MIME::Types.  Meta-POD processed with
+# OODoc into POD and HTML manual-pages.  See README.md
+# Copyright Mark Overmeer.  Licensed under the same terms as Perl itself.
 
 package MIME::Types;
 
--- lib/MojoX/MIME/Types.pm
+++ lib/MojoX/MIME/Types.pm
@@ -1,3 +1,7 @@
+# This code is part of distribution MIME::Types.  Meta-POD processed with
+# OODoc into POD and HTML manual-pages.  See README.md
+# Copyright Mark Overmeer.  Licensed under the same terms as Perl itself.
+
 package MojoX::MIME::Types;
 use Mojo::Base -base;
 
