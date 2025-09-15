2db541c23c76e1ba9d596cb514e0f492d4f4822b
Mark Overmeer <mark@overmeer.net>
2025-09-15 09:23:32 +0200

    fix defaultCharset()

--- lib/MIME/Type.pm
+++ lib/MIME/Type.pm
@@ -1,35 +1,38 @@
-# This code is part of distribution MIME::Types.  Meta-POD processed with
-# OODoc into POD and HTML manual-pages.  See README.md
-# Copyright Mark Overmeer.  Licensed under the same terms as Perl itself.
+#oodist: *** DO NOT USE THIS VERSION FOR PRODUCTION ***
+#oodist: This file contains OODoc-style documentation which will get stripped
+#oodist: during its release in the distribution.  You can use this file for
+#oodist: testing, however the code of this development version may be broken!
 
 package MIME::Type;
 
 use strict;
+use warnings;
 
 use Carp 'croak';
 
+#--------------------
 =chapter NAME
 
 MIME::Type - description of one MIME type
 
 =chapter SYNOPSIS
 
- use MIME::Types;
- my $mimetypes = MIME::Types->new;
- my MIME::Type $plaintext = $mimetypes->type('text/plain');
- print $plaintext->mediaType;   # text
- print $plaintext->subType;     # plain
+  use MIME::Types;
+  my $mimetypes = MIME::Types->new;
+  my MIME::Type $plaintext = $mimetypes->type('text/plain');
+  print $plaintext->mediaType;   # text
+  print $plaintext->subType;     # plain
 
- my @ext = $plaintext->extensions;
- print "@ext"                   # txt asc c cc h hh cpp
+  my @ext = $plaintext->extensions;
+  print "@ext"                   # txt asc c cc h hh cpp
 
- print $plaintext->encoding     # 8bit
- if($plaintext->isBinary)       # false
- if($plaintext->isAscii)        # true
- if($plaintext->equals('text/plain') {...}
- if($plaintext eq 'text/plain') # same
+  print $plaintext->encoding     # 8bit
+  if($plaintext->isBinary)       # false
+  if($plaintext->isText)         # true
+  if($plaintext->equals('text/plain') {...}
+  if($plaintext eq 'text/plain') # same
 
- print MIME::Type->simplified('x-appl/x-zip') #  'appl/zip'
+  print MIME::Type->simplified('x-appl/x-zip') #  'appl/zip'
 
 =chapter DESCRIPTION
 
@@ -38,21 +41,23 @@ and HTTP traffic.  Sometimes real knowledge about a mime-type is need.
 Objects of C<MIME::Type> store the information on one such type.
 =cut
 
-#-------------------------------------------
-
+#--------------------
 =chapter OVERLOADED
 
-=overload stringification
+=overload "" stringification
 The stringification (use of the object in a place where a string
 is required) will result in the type name, the same as M<type()>
 returns.
 
 =examples use of stringification
- my $mime = MIME::Type->new('text/html');
- print "$mime\n";   # explicit stringification
- print $mime;       # implicit stringification
- 
-=overload string comparison
+  my $mime = MIME::Type->new('text/html');
+  print "$mime\n";   # explicit stringification
+  print $mime;       # implicit stringification
+
+=overload == numerical comparison
+Compare whether the M<type()> of the objects is the same.
+
+=overload cmp string comparison
 When a MIME::Type object is compared to either a string or another
 MIME::Type, the M<equals()> method is called.  Comparison is smart,
 which means that it extends common string comparison with some
@@ -61,11 +66,10 @@ features which are defined in the related RFCs.
 =cut
 
 use overload
-    '""' => 'type'
-  , cmp  => 'cmp';
+	'""' => 'type',
+	cmp  => 'cmp';
 
-#-------------------------------------------
-
+#--------------------
 =chapter METHODS
 
 =section Initiation
@@ -98,44 +102,43 @@ depends on the type: mimes with as main type C<text/> will default
 to C<quoted-printable> and all other to C<base64>.
 
 =option  system REGEX
-=default system C<undef>
+=default system undef
 Regular expression which defines for which systems this rule is valid.  The
 REGEX is matched on C<$^O>.
 
 =option  charset $charset
-=default charset C<undef>
+=default charset undef
 Specify the default charset for this type.
 
 =error Type parameter is obligatory.
-When a M<MIME::Type> object is created, the type itself must be
-specified with the C<type> option flag.
+When a MIME::Type object is created, the type itself must be
+specified with the P<type> option flag.
 
 =cut
 
 sub new(@) { (bless {}, shift)->init( {@_} ) }
 
 sub init($)
-{   my ($self, $args) = @_;
+{	my ($self, $args) = @_;
 
-    my $type = $self->{MT_type} = $args->{type}
-       or croak "ERROR: Type parameter is obligatory.";
+	my $type = $self->{MT_type} = $args->{type}
+		or croak "ERROR: Type parameter is obligatory.";
 
-    $self->{MT_simplified} = $args->{simplified}
-       || $self->simplified($type);
+	$self->{MT_simplified} = $args->{simplified} || $self->simplified($type);
 
-    $self->{MT_extensions} = $args->{extensions} || [];
+	$self->{MT_extensions} = $args->{extensions} || [];
 
-    $self->{MT_encoding}
-       = $args->{encoding}          ? $args->{encoding}
-       : $self->mediaType eq 'text' ? 'quoted-printable'
-       :                              'base64';
+	$self->{MT_encoding}
+	  = $args->{encoding}          ? $args->{encoding}
+	  : $self->mediaType eq 'text' ? 'quoted-printable'
+	  :    'base64';
 
-    $self->{MT_system}     = $args->{system}  if defined $args->{system};
+	$self->{MT_system}     = $args->{system}  if defined $args->{system};
 	$self->{MT_charset}    = $args->{charset} if defined $args->{charset};
-    $self;
+	$self;
 }
 
-#-------------------------------------------
+#--------------------
 =section Attributes
 
 =method type
@@ -143,7 +146,7 @@ Returns the long type of this object, for instance C<'text/plain'>
 
 =cut
 
-sub type() {shift->{MT_type}}
+sub type() { $_[0]->{MT_type} }
 
 =ci_method simplified [$string]
 Returns the simplified mime type for this object or the specified STRING.
@@ -153,23 +156,23 @@ the C<x-> can disappear.  In many cases, we prefer the simplified version
 of the type.
 
 =examples results of simplified()
- my $mime = MIME::Type->new(type => 'x-appl/x-zip');
- print $mime->simplified;                     # 'appl/zip'
+  my $mime = MIME::Type->new(type => 'x-appl/x-zip');
+  print $mime->simplified;                     # 'appl/zip'
 
- print $mime->simplified('text/PLAIN');       # 'text/plain'
- print MIME::Type->simplified('x-xyz/x-abc'); # 'xyz/abc'
+  print $mime->simplified('text/PLAIN');       # 'text/plain'
+  print MIME::Type->simplified('x-xyz/x-abc'); # 'xyz/abc'
 
 =cut
 
 sub simplified(;$)
-{   my $thing = shift;
-    return $thing->{MT_simplified} unless @_;
+{	my $thing = shift;
+	return $thing->{MT_simplified} unless @_;
 
-    my $mime  = shift;
+	my $mime  = shift;
 
-      $mime =~ m!^\s*(?:x\-)?([\w.+-]+)/(?:x\-)?([\w.+-]+)\s*$!i ? lc "$1/$2"
-    : $mime eq 'text' ? 'text/plain'          # some silly mailers...
-    : undef;
+	$mime =~ m!^\s*(?:x\-)?([\w.+-]+)/(?:x\-)?([\w.+-]+)\s*$!i ? lc "$1/$2"
+	  : $mime eq 'text' ? 'text/plain'          # some silly mailers...
+	  :   undef;
 }
 
 =method extensions
@@ -201,8 +204,7 @@ C<_FRAMED> when the charset is determined by the content.
 
 sub charset()    { $_[0]->{MT_charset} }
 
-#-------------------------------------------
-
+#--------------------
 =section Knowledge
 
 =method mediaType
@@ -214,7 +216,7 @@ to retrieve the same value.  However, that method is deprecated.
 
 =cut
 
-sub mediaType() {shift->{MT_simplified} =~ m!^([\w.-]+)/! ? $1 : undef}
+sub mediaType() { $_[0]->{MT_simplified} =~ m!^([\w.-]+)/! ? $1 : undef }
 sub mainType()  {shift->mediaType} # Backwards compatibility
 
 =method subType
@@ -222,7 +224,7 @@ The sub type of the simplified mime.
 For C<'text/plain'> it will return C<'plain'>.
 =cut
 
-sub subType() {shift->{MT_simplified} =~ m!/([\w+.-]+)$! ? $1 : undef}
+sub subType() { $_[0]->{MT_simplified} =~ m!/([\w+.-]+)$! ? $1 : undef }
 
 =method isRegistered
 Mime-types which are not registered by IANA nor defined in RFCs shall
@@ -234,22 +236,22 @@ method will return false.
 sub isRegistered() { lc shift->{MT_type} !~ m{^x\-|/x\-} }
 
 =method isVendor
-[2.00] Return C<true> when the type is defined by a vendor; the subtype
+[2.00] Return true when the type is defined by a vendor; the subtype
 starts with C<vnd.>
 
 =method isPersonal
-[2.00] Return C<true> when the type is defined by a person for
+[2.00] Return true when the type is defined by a person for
 private use; the subtype starts with C<prs.>
 
 =method isExperimental
-[2.00] Return C<true> when the type is defined for experimental
+[2.00] Return true when the type is defined for experimental
 use; the subtype starts with C<x.>
 =cut
 
 # http://tools.ietf.org/html/rfc4288#section-3
-sub isVendor()       {shift->{MT_simplified} =~ m!/vnd\.!}
-sub isPersonal()     {shift->{MT_simplified} =~ m!/prs\.!}
-sub isExperimental() {shift->{MT_simplified} =~ m!/x\.!  }
+sub isVendor()       { $_[0]->{MT_simplified} =~ m!/vnd\.! }
+sub isPersonal()     { $_[0]->{MT_simplified} =~ m!/prs\.! }
+sub isExperimental() { $_[0]->{MT_simplified} =~ m!/x\.! }
 
 =method isBinary
 Returns true when the type is not known to be text.  See M<isText()>.
@@ -262,8 +264,8 @@ Old name for M<isText()>.
 there is currently no record of attributes in this module... so we guess.
 =cut
 
-sub isBinary() { shift->{MT_encoding} eq 'base64' }
-sub isText()   { shift->{MT_encoding} ne 'base64' }
+sub isBinary() { $_[0]->{MT_encoding} eq 'base64' }
+sub isText()   { $_[0]->{MT_encoding} ne 'base64' }
 *isAscii = \&isText;
 
 =method isSignature
@@ -272,11 +274,11 @@ Returns true when the type is in the list of known signatures.
 
 # simplified names only!
 my %sigs = map +($_ => 1),
-  qw(application/pgp-keys application/pgp application/pgp-signature
-     application/pkcs10 application/pkcs7-mime application/pkcs7-signature
-     text/vCard);
+qw(application/pgp-keys application/pgp application/pgp-signature
+	application/pkcs10 application/pkcs7-mime application/pkcs7-signature
+	text/vCard);
 
-sub isSignature() { $sigs{shift->{MT_simplified}} }
+sub isSignature() { $sigs{ $_[0]->{MT_simplified}} }
 
 =method equals $string|$mime
 Compare this mime-type object with a STRING or other object.  In case of
@@ -284,32 +286,41 @@ a STRING, simplification will take place.
 =cut
 
 sub cmp($)
-{   my ($self, $other) = @_;
-
-    my $type = ref $other
-      ? $other->simplified
-      : (ref $self)->simplified($other);
-
-    $self->simplified cmp $type;
+{	my ($self, $other) = @_;
+	my $type = ref $other ? $other->simplified : (ref $self)->simplified($other);
+	$self->simplified cmp $type;
 }
+
 sub equals($) { $_[0]->cmp($_[1])==0 }
 
 =method defaultCharset
-As per RFC6657, all C<text/*> types must either specify a default charset
-in its IANA registration, or require the charset parameter.  Non-text
+[2.29] As per RFC6657, all C<text/*> types must either specify a default
+charset in its IANA registration, or require the charset parameter.  Non-text
 types may require a charset as well.
 
 It is hard to extract this information from the IANA registration files
 automagically, so is manually maintained.
+
+=example default charset use
+   $charset //= $type->defaultCharset // 'utf-8';
 =cut
 
 my %ctext;
-$ctext{$_} = 'US-ASCII' for qw/plain cql cql-expression cql-identifier css directory dns encaprtp enriched/;
-$ctext{$_} = 'UTF-8'    for qw/cache-manifest calendar csv csv-schema ecmascript/;
+$ctext{$_} = 'US-ASCII'  for qw/plain cql cql-expression cql-identifier css directory dns encaprtp enriched/;
+$ctext{$_} = 'UTF-8'     for qw/cache-manifest calendar csv csv-schema ecmascript/;
 $ctext{$_} = '_REQUIRED' for qw//;
 
 sub defaultCharset()
-{
+{	my $self = shift;
+	my $st   = (lc $self->subType) =~ s/^x-//r;
+	my $default = $ctext{$st} // return undef;
+
+	if($default eq '_REQUIRED')
+	{	warn "MediaType ".($self->subType)." requires an explicit character-set.";
+		return undef;
+	}
+
+	$default;
 }
 
 1;
